cca63698fce4b1d92a95adef346af8feaaa56499
DualWielder <jonomacgregor@gmail.com>
2019-08-07 10:17:48 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+//var name = 'Jonathan Macgregor'
+//console.log(name)
+
+let name = "Nelson"
+name = "NZ"
+name = "Jonathan"
+console.log(name)
+
+const lastName = "Macgregor"
+const firstName = "Jono"
+console.log(lastName)
+console.log(firstName)
